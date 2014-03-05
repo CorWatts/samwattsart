6fd19dd30dc51c54c8185a4ae3a37b59d6ceacc8
Corey Watts <cwatts@brightidea.com>
2014-03-04 19:08:31 -0800

    Getting set up with asset pipeline and new rake

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,8 @@
+//= require vendor/jquery
+//= require vendor/jquery.color
+//= require vendor/easing
+//= require vendor/jquery.fancybox
+//= require vendor/jquery.fancybox-buttons
+//= require vendor/jquery.fancybox-media
+//= require vendor/jquery.fancybox.pack
+//= require vendor/jquery.fancybox-thumbs
